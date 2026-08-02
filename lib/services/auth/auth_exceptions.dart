f398e3461380c29ddc7bedc6377449da00d40a95
Natnael Zerihun <nathyzer21@gmail.com>
2026-08-02 23:48:58 +0300

    unit testing our app

--- lib/services/auth/auth_exceptions.dart
+++ lib/services/auth/auth_exceptions.dart
@@ -2,6 +2,7 @@ class UserNotFoundAuthException implements Exception {}
 class WrongPasswordAuthException implements Exception {}
 
 
+
 class WeakPasswordAuthException implements Exception {}
 class EmailAlreadyInUseAuthException implements Exception {}
 class InvalidEmailAuthException implements Exception {}
